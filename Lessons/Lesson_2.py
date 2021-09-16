94add016da76a438b1660e2364b1a6a3ea980440
Oalimana <alimana@live.ru>
2021-09-16 20:01:03 +0700

    добавила решение задания 2

--- Lessons/Lesson_2.py
+++ Lessons/Lesson_2.py
@@ -2,20 +2,29 @@
 # num = 7
 # num_1 = 4.8
 # my_name = "Ольга"
-# my_list = [num, num_1, my_name]
+# com = complex(10, 15)
+# my_list = [num, num_1, my_name, com]
 # print(my_list)
 # for el in my_list:
 #     print(f'{el} это {type(el)}')
 
 #Задание №2
-val = input("введите целое число:")
-try:
-    val != int(val)
-except ValueError:
-    print("Вы ввели не целое число, попробуйте еще раз")
-val = input("введите целое число")
-while val != int(val):
-    print("Идите учить математику раз вы не знаете, что такое целое число. Пока")
-print("Хотите ввести еще одно число? Да/нет")
-
+val = (input("Введите любые символы через пробел:")).split()
+my_list = val
+if len(my_list) % 2 == 0:
+        a = 0
+        while a < len(my_list):
+            el = my_list[a]
+            my_list[a] = my_list[a + 1]
+            my_list[a + 1] = el
+            a += 2
+else:
+        a = 0
+        while a < len(my_list) - 1:
+            el = my_list[a]
+            my_list[a] = my_list[a + 1]
+            my_list[a + 1] = el
+            a += 2
+print(my_list)
+#Задание № 3
 
