04419a4f1a83adf4c304ffa9525e2ee8c82f717a
Oalimana <alimana@live.ru>
2021-09-19 00:56:54 +0700

    добавлено задание 4

--- Lessons/Lesson_2.py
+++ Lessons/Lesson_2.py
@@ -26,7 +26,7 @@
 #             my_list[a + 1] = el
 #             a += 2
 # print(my_list)
-#Задание № 3
+'''#Задание № 3
 seasons = [
     ['Зима', ['12', '1', '2']],
     ['Весна', ['3', '4', '5']],
@@ -40,10 +40,20 @@ seasons_dict = {
     'Осень': ['9', '10', '11']
 }
 month_numeral = input('Пожалуйста, введите номер месяца: ')
-for season, months in seasons:# с испоьзованием списка
-    if month_numeral in months:
-        print(f'Вы ввели месяц с номером {month_numeral} это {season}')
-for season, months in seasons_dict.items():#с использованием словаря
-    if month_numeral in months:
-        print(f'Месяц с номером {month_numeral} это {season}')
+if month_numeral > '12':
+    print("Вы ввели неверное число")
+else:
+    for season, months in seasons:# с использованием списка
+        if month_numeral in months:
+            print(f'Вы ввели месяц с номером {month_numeral} это {season}')
+    for season, months in seasons_dict.items():#с использованием словаря
+        if month_numeral in months:
+            print(f'Месяц с номером {month_numeral} это {season}')
+'''
+#Задание № 4
+worlds = (input("Введите любые слова через пробел:")).split()
+my_list = worlds
+for idx, word in enumerate(my_list):
+    print(idx + 1, (word, word[:11])[len(word) > 10])
+
 
